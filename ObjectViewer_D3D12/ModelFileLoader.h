3ad32581c08f525b117cb0355df4643346436e5d
KoshiroShiraki <k22946989@gmail.com>
2021-09-09 22:05:28 +0900

    add OBJ file

---
+++ ObjectViewer_D3D12/ModelFileLoader.h
@@ -0,0 +1,15 @@
+/*
+☆3DFileLoader
+3Dモデルファイルを読み込み、解析しDirectX12のグラフィクスパイプラインへ流せる形へ変換する
+*/
+
+#pragma once
+#include<vector>
+#include<DirectXMath.h>
+
+using namespace DirectX;
+
+class ModelFileLoader {
+public:
+
+};
